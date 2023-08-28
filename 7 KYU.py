fa7373ca8c65545eac382df1f422fbadb6501384
Abdullah Shethwala <ruzruk7@gmail.com>
2023-08-28 15:16:58 -0400

    updated

--- 7 KYU.py
+++ 7 KYU.py
@@ -155,3 +155,41 @@ def series_sum(n):
     return '%.2f' % sum
 
 # PROBLEM 7
+# DESCRIPTION:
+# The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+# To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+# Input
+# Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+# Output
+# Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+# Example
+# input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+# output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+def open_or_senior(data):
+    for pair in data:
+        if pair[0] >= 55 and pair[1] > 7:
+            ind = data.index(pair)
+            data[ind] = "Senior"
+        elif pair[0] < 55 or pair[1] <= 7:
+            ind = data.index(pair)
+            data[ind] = "Open"
+    return data
+
+def openOrSenior(data):
+  return ["Senior" if age >= 55 and handicap >= 8 else "Open" for (age, handicap) in data]
+
+# PROBLEM 8
+# DESCRIPTION:
+# In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+# Example
+# filter_list([1,2,'a','b']) == [1,2]
+# filter_list([1,'a','b',0,15]) == [1,0,15]
+# filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+def filter_list(l):
+    'return a new list with the strings filtered out'
+    return ([x for x in l if type(x) == int])
